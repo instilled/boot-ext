441180018aed148c67676172eb11ae64c3bad062
Fabio <me@instilled.org>
2016-01-16 19:21:37 +0100

    doc update.

--- src/main/clojure/instilled/boot/task.clj
+++ src/main/clojure/instilled/boot/task.clj
@@ -9,7 +9,10 @@
    [java.io File]))
 
 (core/deftask sign-with-keybase
-  "Rebind `boot.gpg/sign-jar` to `instilled.boot.keybase/sign-jar`."
+  "Rebind `boot.gpg/sign-jar` to `instilled.boot.keybase/sign-jar`.
+   This task should be invoked right before push as it rebinds
+   `boot.gpg/sign-jar` permanently. Something along these lines:
+   `boot build sign-with-keybase push`."
   []
   (alter-var-root #'gpg/sign-jar (fn [v r] r) keybase/sign-jar)
   identity)
